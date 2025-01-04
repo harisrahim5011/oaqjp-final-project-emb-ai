e8768855c4b11e3950bb91123f26e2bc41b0ca2c
harisrahim5011 <harisrahim5011@gmail.com>
2025-01-04 05:59:28 -0500

    app deployed on host=0.0.0.0 & port=5000, running successfully

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask('emotion detector app')
+
+@app.route("/")
+def emotionDetector():
+    result_1 = emotion_detector('I think I am having fun')
+    emotion_values_str = ", ".join(f"'{emotion}': {value}" for emotion, value in result_1.items() if emotion != "dominant_emotion") 
+    formatted_output = f"For the given statement, the system response is {emotion_values_str}. The dominant emotion is {result_1['dominant_emotion']}."
+    return formatted_output
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

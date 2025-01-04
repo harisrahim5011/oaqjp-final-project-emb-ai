9181365fced08427e7728a6c8995f7fa7b986c94
harisrahim5011 <harisrahim5011@gmail.com>
2025-01-04 08:18:02 -0500

    static code analysis with pylint is scored 10/10 on server.py file

--- server.py
+++ server.py
@@ -1,17 +1,27 @@
+'''import module from flask
+'''
 from flask import Flask
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask('emotion detector app')
 
 @app.route("/")
-def emotionDetector():
-    result_1 = emotion_detector('')   #'I think I am having fun'
-    emotion_values_str = ", ".join(f"'{emotion}': {value}" for emotion, value in result_1.items() if emotion != "dominant_emotion") 
-    formatted_output = f"For the given statement, the system response is {emotion_values_str}. The dominant emotion is {result_1['dominant_emotion']}."
-    if result_1['dominant_emotion'] == None:
+def emot_detector():
+    '''get the json formatted result, exclude 'dominant' and show seperately 
+    '''
+    result_1 = emotion_detector('')   # I think I am having fun
+    emotion_values_str = ", ".join(f"'{emotion}': {value}"
+                                   for emotion, value in result_1.items()
+                                   if emotion != "dominant_emotion"
+                                    )
+    formatted_output = (f"For the given statement, the system response is "
+                        f"{emotion_values_str}. "
+                        f"The dominant emotion is {result_1['dominant_emotion']}."
+                        )
+    if result_1['dominant_emotion'] is None:
         return 'Invalid text! Please try again!.'
+
     return formatted_output
-    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
